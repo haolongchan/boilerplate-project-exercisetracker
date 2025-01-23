43313eb154ab8a3b3038524aa32bbfbc0a1093d0
Haolong <chl123.chenhaolong@gmail.com>
2025-01-23 02:32:02 -0500

    test

--- index.js
+++ index.js
@@ -43,9 +43,28 @@ app.get('/api/users', async(req, res) => {
 app.post('/api/users/:_id/exercises', async (req, res) => {
   const id = req.params._id;
   const description = req.body.description;
-  const duration = req.body.duration;
-  const dateNumber = req.body.date;
-  const date = (new Date(dateNumber.substr(0, 4) + '-' + dateNumber.substr(4, 2) + '-' + dateNumber.substr(6))).toDateString();
+  const duration = parseInt(req.body.duration);
+  let date;
+  if(!req.body.date){
+    date = new Date().toDateString();
+  }
+  else{
+    
+    let now = new Date(req.body.date);
+    if(isNaN(now) || now.toString() === 'Invalid Date') date = "Thu Jan 01 1970";
+    else{
+        
+      const utcDayOfWeek = now.getUTCDay();
+      const utcMonth = now.getUTCMonth();
+      const utcDate = now.getUTCDate();
+      const utcFullYear = now.getUTCFullYear();
+
+      const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+      date = `${daysOfWeek[utcDayOfWeek]} ${months[utcMonth]} ${utcDate} ${utcFullYear}`;
+    }
+  }
   const user = await User.findOne({_id: id});
   if(!user) return res.json({ error: 'No user found' });
   const exercise = new User({
@@ -57,6 +76,42 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
   });
   await exercise.save();
   res.json({_id: id, username: user.username, date: date, duration: duration, description: description});
+})
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const id = req.params._id;
+  const from = new Date(req.query.from);
+  const to = new Date(req.query.to);
+  const limit = parseInt(req.query.limit);
+  let count = 0;
+  let info = await User.find({id: id}).select({description: 1, duration: 1, date: 1});
+  if(!info) return res.json({ error: 'No user found' });
+  let obj = {
+    _id: id,
+    username: info.username,
+    count: info.length,
+    log: []
+  };
+  for(let i = 0; i < info.length; i++) {
+    const now = new Date(info[i].date);
+    if(req.query.from){
+      if(now < from) continue;
+    }
+    if(req.query.to){
+      if(now > to) continue;
+    }
+    obj.log[count] = {
+      description: info[i].description,
+      duration: parseInt(info[i].duration),
+      date: info[i].date
+    };
+    count++;
+    if(req.query.limit){
+      if(count >= limit) break;
+    }
+  }
+  obj.count = count;
+  res.json(obj);
 })
 
 
